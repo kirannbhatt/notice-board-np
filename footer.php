1a3c7353ac805eca96d7e07ad1258d2c158d1afe
Kiran Bhatt <kb_43@outlook.com>
2019-03-06 21:50:15 +0545

    footer file

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="./vendor/jquery/jquery.min.js"></script>
+  <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="./vendor/script.js"></script>
+</body>
+
+</html>
