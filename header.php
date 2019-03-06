f8cd8c9167d502dc702392d1f39ea78042e2624e
Kiran Bhatt <kb_43@outlook.com>
2019-03-06 21:50:02 +0545

    header file

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>भीमदत्त नगरपालिका नगर कार्यपालिकाको कार्यालय</title>
+  <!-- Bootstrap core CSS -->
+  <link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="./vendor/style.css">
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <div class="container-fluid">
+  <nav class="navbar navbar-light" style="border-bottom: 1px solid #ccc; height: 130px;">
+    <a class="navbar-brand" href="">
+      <img src="./vendor/images/Clogo.png" height="80px" alt="भीमदत्त नगरपालिका नगर कार्यपालिकाको कार्यालय">
+    </a>
+    <div class="my-2 my-lg-0">
+      <img src="./vendor/images/nepal.gif" height="80px" alt="भीमदत्त नगरपालिका नगर कार्यपालिकाको कार्यालय">
+    </div>
+  </nav>
+  <!-- Page Content -->
+  <div class="container-fluid">
