c437ec6db6ba72b58bac59bd3dfbae4db5c8360f
Kiran Bhatt <kb_43@outlook.com>
2019-03-08 08:20:09 +0545

    admin header and footer files

---
+++ admin/dashboard/adminHeader.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../../vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../adminStyles.css" >
+  <title>Admin-भीमदत्त नगरपालिका नगर कार्यपालिकाको कार्यालय</title>
+</head>
+<body>
+
+<nav class="navbar navbar-light" style="border-bottom: 1px solid #ccc;">
+    <a class="navbar-brand" href="../index.php">
+      <img src="../../vendor/images/Clogo.png" height="65px" alt="भीमदत्त नगरपालिका नगर कार्यपालिकाको कार्यालय">
+    </a>
+    <div class="my-2 my-lg-0">
+        <span class="nav-item">
+            <a class="nav-link" href="../dashboard/logout.php">Logout</a>
+        </span>
+    </div>
+</nav>
